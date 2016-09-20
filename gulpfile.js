8fa22d0e31d24cf3db6eace469127993f70f39a2
Frango <leandroscunha@gmail.com>
2016-09-20 09:42:53 -0300

    Added Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,52 @@
-var gulp = require('gulp');
+var gulp 		= require('gulp'),
+	livereload  = require('gulp-livereload'),
+	sourcemaps  = require('gulp-sourcemaps'),
+	stylus 		= require('gulp-stylus');
 
-var paths = {};
+var paths = {
+	css: 'assets/css/',
+	stylus: {
+		src: 'src/stylus/app.styl',
+		watch: 'src/stylus/**/*.styl'
+	}
+};
+
+function buildStylus(options) {
+  var build = function() {
+    gulp
+     .src(options.src)
+     .pipe(sourcemaps.init())
+     .pipe(stylus({
+        compress: options.uglify,
+        use: [],
+        "include css": options.includeCss
+      }))
+     .pipe(sourcemaps.write('.'))
+     .pipe(gulp.dest(options.dest))
+     .pipe(livereload());
+  }
+
+  return build();
+}
 
 gulp.task('somenthing', function() {
 	console.log('Hi, Gulp!')
 });
 
-gulp.task('default', ['somenthing']);
+gulp.task('stylus-dev', function() {
+  return buildStylus({
+    includeCss: true,
+    dest: paths.css,
+    src: paths.stylus.src,
+    uglify: false,
+    watch: true
+  });
+});
+
+gulp.task( 'watch', function() {
+  livereload.listen();
+  gulp.watch(paths.stylus.watch, ['stylus-dev']);
+})
+
+gulp.task('default', ['stylus-dev', 'watch']);
 gulp.task('build', ['somenthing']);
