b23545a454528501e6ef3102a2ce8647ada2e188
Leandro Cunha <leandroscunha@gmail.com>
2018-03-10 19:00:37 -0300

    Added Webpack-Dev-Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
 	entry: './src/app.js',
 	output: {
 		filename: '[name].bundle.js'
